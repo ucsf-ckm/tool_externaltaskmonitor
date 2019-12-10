5fc94b0eb2202dd5996eb5cdb831c50134ee85ae
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-12-10 11:00:25 -0800

    require M3.7 as baseline version.

--- version.php
+++ version.php
@@ -7,6 +7,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2019100900;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2018112800;        // Requires this Moodle version
+$plugin->version   = 2019121000;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2019052000;        // Requires this Moodle version
 $plugin->component = 'tool_externaltaskmonitor';      // Full name of the plugin (used for diagnostics)
