277349ce9adebed544e8ea9b2bf87b0819f7bd0c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-11-01 09:25:50 -0700

    define service in code, makes it easier with installation/setup.

--- db/services.php
+++ db/services.php
@@ -8,6 +8,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+const EXTERNAL_TASK_MONITOR_SERVICE = 'externaltaskmonitor';
+
 $functions = array(
     'tool_externaltaskmonitor_get_scheduled_tasks' => array(
         'classname' => 'tool_externaltaskmonitor\external\monitor',
@@ -16,5 +18,15 @@ $functions = array(
         'type' => 'read',
         'capabilities' => 'moodle/site:config',
         'ajax' => false,
+        'services' => [EXTERNAL_TASK_MONITOR_SERVICE],
     ),
 );
+
+$services = [
+    'External task monitor'  => [
+        'functions' => [],
+        'enabled' => 0,
+        'restrictedusers' => 1,
+        'shortname' => EXTERNAL_TASK_MONITOR_SERVICE,
+    ],
+];
--- version.php
+++ version.php
@@ -7,6 +7,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2022110100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2022110101;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2022041200;        // Requires this Moodle version
 $plugin->component = 'tool_externaltaskmonitor';      // Full name of the plugin (used for diagnostics)
